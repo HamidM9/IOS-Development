b0d19952f150643a328992ad1438d3cd5e87c769
Hamid <hamidqpi@gmail.com>
2023-04-16 00:40:44 +0200

    updateUI

--- Quiz-app/Quiz-app/Controller/ViewController.swift
+++ Quiz-app/Quiz-app/Controller/ViewController.swift
@@ -12,23 +12,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     
-  let  quiz = ["one + two", "TRUE"]
-    
+    let  quiz = [["one + two is 3", "TRUE"],
+                    ["three plus three is six", "TRUE"],
+                    ["Two plus two is nine ", "FALSE"]]
+    var questionNumber = 0
+    var score = 0
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateUI()
     }
 
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
         var userAnswer = sender.currentTitle!
-        print(userAnswer)
+        if userAnswer==quiz[questionNumber][1]{
+            score = score + 1
+            
+        } else{
+            
+        }
+        updateUI()
     }
-    
+    func updateUI(){
+        questionLabel.text = quiz[0][0]
+        if questionNumber+1 < quiz.count{
+            questionLabel.text = quiz[questionNumber][0]
+            questionNumber = questionNumber + 1
+            print(score)
+        }
+    }
     
 }
 
