5a09374e8b8e3a41855a55ed0af886124c56ab4b
Hamid <hamidqpi@gmail.com>
2023-04-15 14:54:57 +0200

    second quiz-app commit

--- Quiz-app/Quiz-app/ViewController.swift
+++ Quiz-app/Quiz-app/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+  let  quiz = ["one + two", "3"]
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional 
+        questionLabel.text = quiz[0]
     }
 
 
