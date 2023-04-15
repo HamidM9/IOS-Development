434db058eddf828c84153e02c586fcbde18c291d
Hamid <hamidqpi@gmail.com>
2023-04-15 14:32:39 +0200

    first quia-app commit

---
+++ Quiz-app/Quiz-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz-app
+//
+//  Created by Hamid Mohammadi on 14/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional 
+    }
+
+
+}
+
