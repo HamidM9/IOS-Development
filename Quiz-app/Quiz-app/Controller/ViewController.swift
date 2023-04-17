5a31cad7eb28f240798fc2cefe1e3c190a94f424
Hamid <hamidqpi@gmail.com>
2023-04-17 14:20:00 +0200

    restard button is added but not working properly- MVC is the next commit :)

--- Quiz-app/Quiz-app/Controller/ViewController.swift
+++ Quiz-app/Quiz-app/Controller/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButtonLabel: UIButton!
     
     @IBOutlet weak var progressBar: UIProgressView!
+   
+    
+    @IBOutlet weak var restartButton: UIButton!
     
     var questionNumber = 0
     var score = 0
@@ -47,21 +50,26 @@ class ViewController: UIViewController {
        
         if questionNumber + 1 < quiz.count  {
             questionNumber = questionNumber + 1
-
+            Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         }
         else{
-            questionNumber = 0
-            score = 0
+            Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(finalUI), userInfo: nil, repeats: false)
+            questionLabel.font = questionLabel.font?.withSize(50)
+            questionLabel.text = "Your Score is \(score)"
+            questionLabel.textAlignment = .center
+            questionLabel.sizeToFit()
+            
+            scoreLabel.text =  "Score : \(score)"
+            restartModule()
         }
-        
-        
-         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
-        
-        
-        
         
         
     }
+    
+    @IBAction func restartButtonPressed(_ sender: UIButton) {
+    }
+    
+    
    @objc func updateUI(){
         print("--------------------------")
         questionLabel.text = quiz[questionNumber].text
@@ -73,6 +81,24 @@ class ViewController: UIViewController {
        progressBar.progress = Float(questionNumber+1) / Float(quiz.count)
         
 
+    }
+    
+    @objc func finalUI(){
+        
+        
+        scoreLabel.isHidden = true
+        trueButtonLabel.backgroundColor = UIColor.systemGroupedBackground
+        falseButtonLabel.backgroundColor = UIColor.systemGroupedBackground
+    }
+    
+    func restartModule(){
+        print("ok")
+        trueButtonLabel.isHidden = true
+        falseButtonLabel.isHidden = true
+        restartButton.isHidden = false
+        restartButton.layer.cornerRadius = restartButton.frame.width / 2
+        restartButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        restartButton.layer.masksToBounds = true
     }
     
 }
