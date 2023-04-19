269fccd8ca0096cdb75255ab3776ac6a1586e640
Hamid <hamidqpi@gmail.com>
2023-04-19 19:28:20 +0200

    creating new project

---
+++ mock-your-destiny/mock-your-destiny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mock-your-destiny
+//
+//  Created by Hamid Mohammadi on 19/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
